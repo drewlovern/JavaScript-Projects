6b8af0c719c25cd6348b899ebbba4aedd9fe483c
drewlovern <114452716+drewlovern@users.noreply.github.com>
2022-10-10 13:37:43 -0700

    Project 1 Uploaded

---
+++ Project1_expressions_alert/JS/main.js
@@ -0,0 +1,20 @@
+window.alert("Hello, World!");
+document.write('Hello, World!');
+var X ="Check it out!"; // Assigning a variable
+document.write("<br>");
+document.write(X);
+window.alert("Here we go!");
+var Y = "Take off!"; // Assigning a variable
+document.write("<br>");
+document.write(Y);
+document.write ("I told him, \"Get outta here\" but he didn't leave.");
+document.write ("This is the beginning of the string"    // Concatenating strings
++" and this is the end of the string.");
+document.write("<br>");
+var B = "Concatenated" + " String" ; // Assigning a variable
+document.write (B);  // Assigning a variable
+document.write("<br>");
+var Hot = "Ouch!", Cold = "Brrr!", Lukewarm = "Niiiice!" ;
+document.write (Cold);
+document.write("<br>");
+document.write (4+7);

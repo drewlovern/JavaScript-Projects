baf3b92b0c85098dab7924db2563c6c76f094b31
drewlovern <114452716+drewlovern@users.noreply.github.com>
2022-10-10 15:06:44 -0700

    Project 2

---
+++ Project2_functions/JS/main.js
@@ -0,0 +1,13 @@
+function function_1 () {
+    var str = "Hey would you look at that!";
+    var str2 = " It worked!";
+    document.getElementById("F1") .innerHTML = str+str2;
+}
+var X = "You did it!";
+document.write(X);
+
+function myfunction() {
+    var sentence = "I am learning";
+    sentence += " a lot from this course!";
+    document.getElementByID("Concatenate").innerHTML = sentence;
+}
